83cd4af2cd377f8b920e26c977eb3f9d2a537da5
Witherman17 <31626399+retropieman@users.noreply.github.com>
2020-04-15 13:19:54 -0600

    Update loader.js

--- games/loader.js
+++ games/loader.js
@@ -1,5 +1,5 @@
-games = null;
-$.get("games/games.json", function(data, status){
+
+$.getJson("games/games.json", function(data, status){
     if(status == 200){
       games = JSON.parse(data);
     }else{
