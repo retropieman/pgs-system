8e5599b9d124660acf2987e9f25278dde6af2eb2
Witherman17 <31626399+retropieman@users.noreply.github.com>
2020-04-15 13:39:03 -0600

    Update loader.js

--- games/loader.js
+++ games/loader.js
@@ -2,11 +2,16 @@
 var _games = $.getJSON("games/games.json", function(data, status){
     if(status == 200){
       _mygames = JSON.parse(data);
+      $( "#flags" ).append( "<div id='loaded'></div>" );
+      
     }else{
       alert(status + ": There was an error loading your games.");
     }
 });
-mygames = _games.responseText;
-games = JSON.parse(mygames);
 
 
+$("#flags").on('ready', '#loaded', function(){
+    mygames = _games.responseText;
+    games = JSON.parse(mygames);
+
+});
